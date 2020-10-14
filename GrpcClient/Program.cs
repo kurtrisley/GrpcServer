92388067d56a97a3c921995a75da6c188db6787f
Kurt Risley <35821759+kurtrisley@users.noreply.github.com>
2020-10-14 14:32:01 -0600

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Calling GRPC Service");
+            Console.Write("Hit enter to do the deed");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001"); 
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Putintane" });
+
+            Console.WriteLine($"Got the response {reply.Message}");
+
+            Console.WriteLine($"Got the response {reply.Message}");
+
+            Console.WriteLine("Planning your route ...");
+
+            var clientRouting = new DrivingRouter.DrivingRouterClient(channel);
+            var request = new RouteRequest { Street = "555 Mockingbird Ct.", City = "Des Moines", Zip = "23892" };
+            var replyRoute = await clientRouting.PlanRouteAsync(request);
+
+            Console.WriteLine("Arriving at " + replyRoute.ArrivalTime);
+            Console.WriteLine("Miles: " + replyRoute.Miles);
+            foreach(var step in replyRoute.Steps)
+            {
+                Console.WriteLine($"\t{step}");
+            }
+        }
+    }
+}
---
+++ GrpcServer/Services/RoutingService.cs
@@ -0,0 +1,23 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class RoutingService : DrivingRouter.DrivingRouterBase
+    {
+        public override async Task<RouteResponse> PlanRoute(RouteRequest request, ServerCallContext context)
+        {
+            var response = new RouteResponse();
+            response.Miles = new Random().Next(15, 200);
+            response.Steps.Add("At the end of your driveway, go left");
+            response.Steps.Add("Turn right at Darrow");
+            response.Steps.Add("Keep going until you get to the Pacific");
+            response.Steps.Add("You missed it - " + request.Street);
+            var time = Timestamp.FromDateTime(DateTime.Now.AddHours(15));
+            response.ArrivalTime = DateTime.Now.AddHours(15).ToString();
+            return response;
+        }
+    }
+}
